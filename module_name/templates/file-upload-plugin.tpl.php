20ce2ff6e784e0ac62915bc60249ae7ba2abaf9f
Sergei Semipyadniy <sergei.semipyadniy@ffwagency.com>
2016-10-21 15:23:00 +0300

    Init.

---
+++ module_name/templates/file-upload-plugin.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * File upload template.
+ */
+?>
+
+<div class="outer-classes">
+  <div class="inner-class">
+
+    <div class="specific-class">
+      <?php print $file_upload_title; ?>
+    </div>
+
+    <?php print $file_upload_image; ?>
+    <?php print $file_upload_body; ?>
+    <?php print $file_upload_button; ?>
+  </div>
+</div>
